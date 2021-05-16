c3b75399862eb04737cd84bd3d8ea1df622cd970
MPA81 <mpantonescu81@gmail.com>
2021-05-15 20:08:53 -0700

    Sat 05.15.21  Flawed, does not do what it should.

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import os
+from random import randrange
+
 
 def display_board(board):
+    # The function accepts one parameter containing the board's current status
+    # and prints it out to the console.
+
+    global moves
+    moves = 0
+
     os.system('clear') 
     print(3 * "+-------", end="+\n")
     for i in range(3): 
@@ -17,39 +25,44 @@ def display_board(board):
     if victor:
         return
 
-    enter_move(board)
+    if moves % 2:
+        moves += 1
+        draw_move(board)
+    else:
+        moves += 1
+        enter_move(board)
 
-    # The function accepts one parameter containing the board's current status
-    # and prints it out to the console.
 
 
 def enter_move(board):
-    chango = int(input("Tell us the number of the square you would like to place an X in: "))
-    if chango < 10:
-        chango -= 1
-    x = chango // 3
-    y = chango % 3
-    board[x][y] = "X"
-    display_board(board)
-    make_list_of_free_fields(board)
-
     # The function accepts the board current status, asks the user about their move,
     # checks the input and updates the board according to the user's decision.
 
+    chango = int(input("Tell us the number of the square you would like to place an X in: "))
+    if chango < 10:
+        chango -= 1
+
+    move_helper(board, chango, "X")
+
 
 def make_list_of_free_fields(board):
+    # The function browses the board and builds a list of all the free squares;
+    # the list consists of tuples, while each tuple is a pair of row and column numbers.
+
     available = []
     for i in board:
         for j in range(1,10):
             if j in i:
                 available.append(j) 
     return available 
-    # The function browses the board and builds a list of all the free squares;
-    # the list consists of tuples, while each tuple is a pair of row and column numbers.
 
 
 def victory_for(board, sign):
+    # The function analyzes the board status in order to check if
+    # the player using 'O's or 'X's has won the game
+
     xos = []
+    booly = False 
     for i in range(3):
         for j in range(3):
             if board[i][j] == sign:
@@ -57,26 +70,58 @@ def victory_for(board, sign):
                 xos.append(posi) 
 
     print(xos)
-    '''
-    if 
    
-    ### there is a total of 8 possible combinations of squares that will form a victory
-    # for either sign. Of these 3 are vertical, 3 are horizontal and 2 lay diagonaly
-    # accross the board. some of these possibilities can be grouped together with 'and'
-    # 'or' combinations.  for example out of the top [0,0] corner we have 3 distinct
-    # posibilities, one vertical, one horizontal and one diagonal.  The same is true for
-    # the bottom far corner [3,3]. with these 6 possibilities out of the way, there
-    # remains only one vertical and one horizontal posibility to contend with.
+    if (1,1)in xos:
+        if (0,2) and (2,0) in xos:
+            booly = True
+        elif (1,0) and (1,2) in xos:
+            booly = True
+        elif (0,1) and (2,1) in xos:
+            booly = True
+        elif (0,0) and (2,2) in xos:
+            booly = True
+    
+    if (0,0) in xos:
+        if (0,1) and (0,2) in xos:
+            booly = True
+        elif (1,0) and (2,0) in xos:
+            booly = True
 
-    '''
-    return True 
-    # The function analyzes the board status in order to check if
-    # the player using 'O's or 'X's has won the game
+    if (2,2) in xos:
+        if (2,0) and (2,1) in xos:
+            booly = True
+        elif (0,2) and (1,2) in xos:
+            booly = True
 
+    if booly:
+        print(sign, "is the winner of this game!")
+        
+    return booly 
+    
 
 def draw_move(board):
-    return 
     # The function draws the computer's move and updates the board.
+
+    avail = make_list_of_free_fields(board)
+    i = 11
+
+    print(avail, "- available")
+
+    while i not in avail:
+        for j in range(10):
+            i = randrange(5)
+
+    move_helper(board, i, "O")
+
+
+def move_helper(board, val, sign):
+    x = val // 3
+    y = val % 3
+    board[x][y] = sign 
+
+    display_board(board)
+    make_list_of_free_fields(board)
+
 
 # creating the 2D array for the board
 board = [[0 for x in range(3)] for y in range(3)]
@@ -86,10 +131,6 @@ for i in range(9):
     x = i // 3
     y = i % 3
     z = board[x][y] = i + 1
-
-board[1][1] = "X"
-board[0][1] = "O"
-board[2][0] = "X"
 
 print(board)
 display_board(board)
